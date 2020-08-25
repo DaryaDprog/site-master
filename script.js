6c1e40fdfcf459b655f155a933bdae3a9c7c8957
DaryaDprog <61011966+DaryaDprog@users.noreply.github.com>
2020-08-25 18:30:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-let filters = document.querySelectorAll('.filter__title');
+let filters = document.querySelectorAll('.filter__title'),
+    check = document.querySelectorAll('.location-item'),
+    locationBtn = document.querySelector('.location-btn'),
+    row = document.querySelector('.main__row'),
+    mainBtn = document.querySelector('.main__btn'),
+    extraItems = document.querySelector('.item-extra'),
+    items = document.querySelector('.extra-items'),
+    dropFiltersBtn = document.querySelector('.filter-btn--disabled'),
+    all = document.querySelectorAll('input'),
+    form = document.forms['main_form'],
+    l = document.getElementsByName('location');
+
+    /* Render filters */
 
 filters.forEach( filter => {
     filter.addEventListener('click', () => {
@@ -6,72 +18,44 @@ filters.forEach( filter => {
     });
 });
 
+check.forEach((e)=>{
+    e.addEventListener('click', function() {
+        this.classList.toggle('active');
+    });
+});
 
-let check = document.querySelectorAll('.location-item'),
-    locationBtn = document.querySelector('.location-btn');
-
+/* Location drop filter */
+locationBtn.addEventListener('click', function(event) {
+    event.preventDefault();
     check.forEach((e)=>{
-        e.addEventListener('click', function() {
-            this.classList.toggle('active');
-        });
+        e.classList.remove('active');
+        locationBtn.classList.add('active');
     });
-    locationBtn.addEventListener('click', function(event) {
-        event.preventDefault();
-        check.forEach((e)=>{
-            e.classList.remove('active');
-            locationBtn.classList.add('active');
-        });
-    });
+});
 
-
-let row = document.querySelector('.main__row'),
-    mainBtn = document.querySelector('.main__btn');
-
+/* Add new row */
     mainBtn.addEventListener('click', function() {
         event.preventDefault();
-
         let rows = row.cloneNode(true);
         row.after(rows);
     });
 
-let extraItems = document.querySelector('.item-extra'),
-    items = document.querySelector('.extra-items'); 
-
+/* Open extra items is additional filter */
 extraItems.addEventListener('click', function() {
     extraItems.style.display = 'none';
     items.style.display = 'block';
 });
-   
-let dropFiltersBtn = document.querySelector('.filter-btn--disabled');
 
-let all = document.querySelectorAll('input');
-
-let form = document.forms['main_form'];
-let l = document.getElementsByName('location');
-
+/* Drop all filters */
 dropFiltersBtn.addEventListener('click', function(event) {
     event.preventDefault();
 
-               for(let i=0;i<form.elements.length;i++) {
+               for(let i = 0; i < form.elements.length; i++) {
                 if(form.elements[i].checked) {
-                    form.elements[i].checked=false;
+                    form.elements[i].checked = false;
                     l.forEach((el) => {
                         el.classList.remove('active');
                     });
                 }
             }
-   
-
-    // function clearCheckBoxes() {
-    //         var form=document.forms['main_form'];
-            
-    //         for(var i=0;i<form.elements.length;i++) {
-    //             if(form.elements[i].checked) {
-    //                 form.elements[i].checked=false;
-    //             }
-    //         }
-        
-    //     }
-    
-
 });
